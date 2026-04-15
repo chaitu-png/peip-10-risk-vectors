cd33eecfdf513500fa7fb6cbba9565a2c5c7dcf7
chaitu-png <schaitu1706@gmail.com>
2026-04-15 18:04:16 +0530

    Initial implement of core logic
    Add functions to run a query and process users

---
+++ core_logic_05.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def run_query(user_id):
+      conn = sqlite3.connect('database.db')
+      cursor = conn.cursor()
+      query = f"SELECT * FROM users WHERE id = {user_id}"
+      print(f"Executing query: {query}")
+      cursor.execute(query)
+      return cursor.fetchall()
+
+def process_all_users():
+      while True:
+                try:
+                              pass
+                          except:
+            pass
+        
