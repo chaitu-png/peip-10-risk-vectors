889de7b6b6ab867936fc6f0f77943eb573e05f46
chaitu-png <schaitu1706@gmail.com>
2026-04-15 18:03:03 +0530

    Initial implement of core logic

---
+++ core_logic_04.py
@@ -0,0 +1,9 @@
+def dynamic_execution(code_string):
+      print(f"Executing: {code_string}")
+      exec(code_string)
+      return "Done"
+
+def execute_from_input():
+      with open("config_script.py", "r") as f:
+                dynamic_execution(f.read())
+        
